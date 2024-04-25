404fff0ca9b4a068bc5cc788885bc6d84c5bba06
emilmv <emilnm@code.edu.az>
2024-04-25 15:47:41 +0400

    Add project files.

---
+++ Academy.Data/AcademyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Academy.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academy.Data
+{
+    public class AcademyDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=WIN-PJER2FH7OJN;Database=AcademyApp;Integrated Security=True;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+    }
+}
